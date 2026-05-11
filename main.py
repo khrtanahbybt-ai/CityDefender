d0673528037325912950a84fc3f0c90b4fc25c64
khrtanahbybt-ai <khrtanahbybt@gmail.com>
2026-05-11 18:31:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle, Ellipse
+from kivy.core.window import Window
+from random import randint, choice
+import math
+
+WIDTH, HEIGHT = 360, 640
+PLAYER_SIZE = 40
+...
